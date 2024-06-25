8c790d49062a8870f4b6f5c218a3be7c58999c7d
Guheil <xgael.sanjuan@gmail.com>
2024-06-25 16:33:08 +0800

    added add job page

--- job.php
+++ job.php
@@ -4,9 +4,9 @@ session_start();
 include_once 'PHP_Connections\db_connection.php';
 // Check if user is logged in
 if (!isset($_SESSION['username'])) {
-  // Redirect to login page if not logged in
-  header('Location: login.php');
-  exit();
+	// Redirect to login page if not logged in
+	header('Location: login.php');
+	exit();
 }
 
 // Get user's name from session
@@ -18,10 +18,10 @@ $result = $mysqli->query($sql);
 $employee_count = 0;
 
 if ($result) {
-  $row = $result->fetch_assoc();
-  $employee_count = $row['count'];
+	$row = $result->fetch_assoc();
+	$employee_count = $row['count'];
 } else {
-  echo "Error retrieving employee count: " . $mysqli->error;
+	echo "Error retrieving employee count: " . $mysqli->error;
 }
 
 $sql = "SELECT COUNT(*) as count FROM job";
@@ -29,10 +29,10 @@ $result = $mysqli->query($sql);
 $employee_count = 0;
 
 if ($result) {
-  $row = $result->fetch_assoc();
-  $job_count = $row['count'];
+	$row = $result->fetch_assoc();
+	$job_count = $row['count'];
 } else {
-  echo "Error retrieving job count: " . $mysqli->error;
+	echo "Error retrieving job count: " . $mysqli->error;
 }
 
 ?>
@@ -41,189 +41,245 @@ if ($result) {
 <html lang="en">
 
 <head>
-  <meta charset="utf-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0" />
-  <title>DOST-HRMO</title>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0" />
+	<title>DOST-HRMO</title>
 
-  <link rel="shortcut icon" href="assets/img/dost_logo.png" />
+	<link rel="shortcut icon" href="assets/img/dost_logo.png" />
 
-  <link rel="stylesheet" href="assets/css/bootstrap.min.css" />
+	<link rel="stylesheet" href="assets/css/bootstrap.min.css" />
 
-  <link rel="stylesheet" href="assets/plugins/select2/css/select2.min.css" />
+	<link rel="stylesheet" href="assets/plugins/select2/css/select2.min.css" />
 
-  <link rel="stylesheet" href="assets/plugins/fontawesome/css/fontawesome.min.css" />
-  <link rel="stylesheet" href="assets/plugins/fontawesome/css/all.min.css" />
+	<link rel="stylesheet" href="assets/plugins/fontawesome/css/fontawesome.min.css" />
+	<link rel="stylesheet" href="assets/plugins/fontawesome/css/all.min.css" />
 
-  <link rel="stylesheet" href="assets/css/style.css" />
-  <!--[if lt IE 9]>
+	<link rel="stylesheet" href="assets/css/style.css" />
+	<!--[if lt IE 9]>
       <script src="assets/js/html5shiv.min.js"></script>
       <script src="assets/js/respond.min.js"></script>
     <![endif]-->
 </head>
 
 <body>
-  <div class="main-wrapper">
-  <div class="header">
+	<div class="main-wrapper">
+		<div class="header">
 
-<div class="header-left">
-	<div class="logo-wrapper">
-		<a href="index.php" class="logo">
-			<img src="assets/img/DOST.png" alt="Logo">
-		</a>
+			<div class="header-left">
+				<div class="logo-wrapper">
+					<a href="index.php" class="logo">
+						<img src="assets/img/DOST.png" alt="Logo">
+					</a>
 
-	</div>
-
-	<a href="index.php" class="logo logo-small">
-		<img src="assets/img/dost_logo.png" alt="Logo" width="30" height="30">
-	</a>
-	<a href="javascript:void(0);" id="toggle_btn">
-		<span class="bar-icon">
-			<span></span>
-			<span></span>
-			<span></span>
-		</span>
-	</a>
-</div>
-
-<div class="top-nav-search">
-	<form>
-		<input type="text" class="form-control" placeholder="">
-		<button class="btn" type="submit"><i class="fas fa-search"></i></button>
-	</form>
-</div>
-
-
-<a class="mobile_btn" id="mobile_btn">
-	<i class="fas fa-bars"></i>
-</a>
-<ul class="nav user-menu">
-
-	<li class="nav-item dropdown has-arrow main-drop">
-		<a href="#" class="dropdown-toggle nav-link" data-toggle="dropdown">
-			<span class="user-img">
-				<img src="<?php echo htmlspecialchars($profile_image_path); ?>" alt="Avatar" style="border-radius: 50%; width: 45px; height: 45px;">
-				<span class="status online"></span>
-			</span>
-			<span><?php echo htmlspecialchars($user_name); ?></span>
-		</a>
-		<div class="dropdown-menu">
-			<a class="dropdown-item" href="profile.html"><i data-feather="user" class="mr-1"></i>
-				Profile</a>
-			<a class="dropdown-item" href="settings.html"><i data-feather="settings" class="mr-1"></i>
-				Settings</a>
-			<a class="dropdown-item" href="PHP_Connections/logout.php"><i data-feather="log-out" class="mr-1"></i>
-				Logout</a>
-		</div>
-	</li>
-
-</ul>
-<div class="dropdown mobile-user-menu show">
-	<a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" aria-expanded="false"><i class="fa fa-ellipsis-v"></i></a>
-	<div class="dropdown-menu dropdown-menu-right ">
-		<a class="dropdown-item" href="profile.php">My Profile</a>
-		<a class="dropdown-item" href="settings.html">Settings</a>
-		<a class="dropdown-item" href="PHP_Connections/logout.php">Logout</a>
-	</div>
-</div>
-
-</div>
-
-
-<div class="sidebar" id="sidebar">
-<div class="sidebar-inner slimscroll">
-	<div class="sidebar-contents">
-		<div id="sidebar-menu" class="sidebar-menu">
-			<div class="mobile-show">
-				<div class="offcanvas-menu">
-					<div class="user-info align-center bg-theme text-center">
-						<span class="lnr lnr-cross  text-white" id="mobile_btn_close">X</span>
-						<a href="javascript:void(0)" class="d-block menu-style text-white">
-							<div class="user-avatar d-inline-block mr-3">
-								<img src="<?php echo htmlspecialchars($profile_image_path); ?>" alt="user avatar" class="rounded-circle" width="50">
-							</div>
-						</a>
-					</div>
 				</div>
-				<div class="sidebar-input">
-					<div class="top-nav-search">
-						<form>
-							<input type="text" class="form-control" placeholder="Search here">
-							<button class="btn" type="submit"><i class="fas fa-search"></i></button>
-						</form>
+
+				<a href="index.php" class="logo logo-small">
+					<img src="assets/img/dost_logo.png" alt="Logo" width="30" height="30">
+				</a>
+				<a href="javascript:void(0);" id="toggle_btn">
+					<span class="bar-icon">
+						<span></span>
+						<span></span>
+						<span></span>
+					</span>
+				</a>
+			</div>
+
+			<div class="top-nav-search">
+				<form>
+					<input type="text" class="form-control" placeholder="">
+					<button class="btn" type="submit"><i class="fas fa-search"></i></button>
+				</form>
+			</div>
+
+
+			<a class="mobile_btn" id="mobile_btn">
+				<i class="fas fa-bars"></i>
+			</a>
+			<ul class="nav user-menu">
+
+				<li class="nav-item dropdown has-arrow main-drop">
+					<a href="#" class="dropdown-toggle nav-link" data-toggle="dropdown">
+						<span class="user-img">
+							<img src="<?php echo htmlspecialchars($profile_image_path); ?>" alt="Avatar" style="border-radius: 50%; width: 45px; height: 45px;">
+							<span class="status online"></span>
+						</span>
+						<span><?php echo htmlspecialchars($user_name); ?></span>
+					</a>
+					<div class="dropdown-menu">
+						<a class="dropdown-item" href="profile.html"><i data-feather="user" class="mr-1"></i>
+							Profile</a>
+						<a class="dropdown-item" href="settings.html"><i data-feather="settings" class="mr-1"></i>
+							Settings</a>
+						<a class="dropdown-item" href="PHP_Connections/logout.php"><i data-feather="log-out" class="mr-1"></i>
+							Logout</a>
+					</div>
+				</li>
+
+			</ul>
+			<div class="dropdown mobile-user-menu show">
+				<a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" aria-expanded="false"><i class="fa fa-ellipsis-v"></i></a>
+				<div class="dropdown-menu dropdown-menu-right ">
+					<a class="dropdown-item" href="profile.php">My Profile</a>
+					<a class="dropdown-item" href="settings.html">Settings</a>
+					<a class="dropdown-item" href="PHP_Connections/logout.php">Logout</a>
+				</div>
+			</div>
+
+		</div>
+
+
+		<div class="sidebar" id="sidebar">
+			<div class="sidebar-inner slimscroll">
+				<div class="sidebar-contents">
+					<div id="sidebar-menu" class="sidebar-menu">
+						<div class="mobile-show">
+							<div class="offcanvas-menu">
+								<div class="user-info align-center bg-theme text-center">
+									<span class="lnr lnr-cross  text-white" id="mobile_btn_close">X</span>
+									<a href="javascript:void(0)" class="d-block menu-style text-white">
+										<div class="user-avatar d-inline-block mr-3">
+											<img src="<?php echo htmlspecialchars($profile_image_path); ?>" alt="user avatar" class="rounded-circle" width="50">
+										</div>
+									</a>
+								</div>
+							</div>
+							<div class="sidebar-input">
+								<div class="top-nav-search">
+									<form>
+										<input type="text" class="form-control" placeholder="Search here">
+										<button class="btn" type="submit"><i class="fas fa-search"></i></button>
+									</form>
+								</div>
+							</div>
+						</div>
+						<ul>
+							<li>
+								<a href="index.php"><img src="assets/img/home.svg" alt="sidebar_img">
+									<span>Dashboard</span></a>
+							</li>
+							<li>
+								<a href="employee.php"><img src="assets/img/employee.svg" alt="sidebar_img"><span>
+										Employees</span></a>
+							</li>
+							<li>
+								<a href="company.html"><img src="assets/img/company.svg" alt="sidebar_img"> <span>
+										Applicants</span></a>
+							</li>
+							<li class="active">
+								<a href="job.php"><img src="assets/img/calendar.svg" alt="sidebar_img">
+									<span>Jobs</span></a>
+							</li>
+							<li>
+								<a href="leave.html"><img src="assets/img/leave.svg" alt="sidebar_img">
+									<span>Departments</span></a>
+							</li>
+							<li>
+								<a href="review.html"><img src="assets/img/review.svg" alt="sidebar_img"><span>Review</span></a>
+							</li>
+							<li>
+								<a href="report.html"><img src="assets/img/report.svg" alt="sidebar_img"><span>Report</span></a>
+							</li>
+							<li>
+								<a href="manage.html"><img src="assets/img/manage.svg" alt="sidebar_img">
+									<span>Manage</span></a>
+							</li>
+							<li>
+								<a href="settings.html"><img src="assets/img/settings.svg" alt="sidebar_img"><span>Settings</span></a>
+							</li>
+							<li>
+								<a href="profile.php"><img src="assets/img/profile.svg" alt="sidebar_img">
+									<span>Profile</span></a>
+							</li>
+						</ul>
+						<ul class="logout">
+							<li>
+								<a href="PHP_Connections/logout.php"><img src="assets/img/logout.svg" alt="sidebar_img"><span>Log
+										out</span></a>
+							</li>
+						</ul>
 					</div>
 				</div>
 			</div>
-			<ul>
-				<li>
-					<a href="index.php"><img src="assets/img/home.svg" alt="sidebar_img">
-						<span>Dashboard</span></a>
-				</li>
-				<li>
-					<a href="employee.php"><img src="assets/img/employee.svg" alt="sidebar_img"><span>
-							Employees</span></a>
-				</li>
-				<li>
-					<a href="company.html"><img src="assets/img/company.svg" alt="sidebar_img"> <span>
-							Applicants</span></a>
-				</li>
-				<li  class="active">
-					<a href="job.php"><img src="assets/img/calendar.svg" alt="sidebar_img">
-						<span>Jobs</span></a>
-				</li>
-				<li>
-					<a href="leave.html"><img src="assets/img/leave.svg" alt="sidebar_img">
-						<span>Departments</span></a>
-				</li>
-				<li>
-					<a href="review.html"><img src="assets/img/review.svg" alt="sidebar_img"><span>Review</span></a>
-				</li>
-				<li>
-					<a href="report.html"><img src="assets/img/report.svg" alt="sidebar_img"><span>Report</span></a>
-				</li>
-				<li>
-					<a href="manage.html"><img src="assets/img/manage.svg" alt="sidebar_img">
-						<span>Manage</span></a>
-				</li>
-				<li>
-					<a href="settings.html"><img src="assets/img/settings.svg" alt="sidebar_img"><span>Settings</span></a>
-				</li>
-				<li>
-					<a href="profile.php"><img src="assets/img/profile.svg" alt="sidebar_img">
-						<span>Profile</span></a>
-				</li>
-			</ul>
-			<ul class="logout">
-				<li>
-					<a href="PHP_Connections/logout.php"><img src="assets/img/logout.svg" alt="sidebar_img"><span>Log
-							out</span></a>
-				</li>
-			</ul>
+		</div>
+
+		<div class="page-wrapper">
+			<div class="row">
+				<div class="col-md-9 mx-auto my-5">
+					<div class="card">
+						<div class="card-header">
+							<h4 class="card-title">Add New Job</h4>
+						</div>
+						<div class="card-body">
+							<?php if (!empty($success)) : ?>
+								<div class="alert alert-success">
+									<?php echo htmlspecialchars($success); ?>
+								</div>
+							<?php endif; ?>
+
+							<?php if (!empty($errors)) : ?>
+								<div class="alert alert-danger">
+									<ul>
+										<?php foreach ($errors as $error) : ?>
+											<li><?php echo htmlspecialchars($error); ?></li>
+										<?php endforeach; ?>
+									</ul>
+								</div>
+							<?php endif; ?>
+
+							<form method="POST" action="">
+								<div class="form-group">
+									<label for="position">Position</label>
+									<input type="text" name="position" id="position" class="form-control" value="<?php echo isset($_POST['position']) ? htmlspecialchars($_POST['position']) : ''; ?>">
+								</div>
+								<div class="form-group">
+									<label for="description">Description</label>
+									<textarea name="description" id="description" class="form-control"><?php echo isset($_POST['description']) ? htmlspecialchars($_POST['description']) : ''; ?></textarea>
+								</div>
+								<div class="form-group">
+									<label for="department_id">Department</label>
+									<input type="number" name="department_id" id="department_id" class="form-control" value="<?php echo isset($_POST['department_id']) ? htmlspecialchars($_POST['department_id']) : ''; ?>">
+								</div>
+								<div class="form-group">
+									<label for="monthly_salary">Monthly Salary</label>
+									<input type="number" step="0.01" name="monthly_salary" id="monthly_salary" class="form-control" value="<?php echo isset($_POST['monthly_salary']) ? htmlspecialchars($_POST['monthly_salary']) : ''; ?>">
+								</div>
+								<div class="form-group">
+									<label for="daily_salary">Daily Salary</label>
+									<input type="number" step="0.01" name="daily_salary" id="daily_salary" class="form-control" value="<?php echo isset($_POST['daily_salary']) ? htmlspecialchars($_POST['daily_salary']) : ''; ?>">
+								</div>
+								<div class="form-group">
+									<label for="status">Status</label>
+									<select name="status" id="status" class="form-control">
+										<option value="permanent" <?php echo (isset($_POST['status']) && $_POST['status'] == 'permanent') ? 'selected' : ''; ?>>Permanent</option>
+										<option value="cos" <?php echo (isset($_POST['status']) && $_POST['status'] == 'cos') ? 'selected' : ''; ?>>COS</option>
+									</select>
+								</div>
+								<button type="submit" class="btn btn-primary w-25">Add Job</button>
+							</form>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			</form>
 		</div>
 	</div>
-</div>
-</div>
+	<script>
 
-    <div class="page-wrapper">
-      <form action="post">
-		
-	  </form>
-    </div>
-  </div>
-  <script>
+	</script>
+	<script src="assets/js/jquery-3.6.0.min.js"></script>
 
-  </script>
-  <script src="assets/js/jquery-3.6.0.min.js"></script>
+	<script src="assets/js/popper.min.js"></script>
+	<script src="assets/js/bootstrap.min.js"></script>
 
-  <script src="assets/js/popper.min.js"></script>
-  <script src="assets/js/bootstrap.min.js"></script>
+	<script src="assets/js/feather.min.js"></script>
 
-  <script src="assets/js/feather.min.js"></script>
+	<script src="assets/plugins/slimscroll/jquery.slimscroll.min.js"></script>
 
-  <script src="assets/plugins/slimscroll/jquery.slimscroll.min.js"></script>
+	<script src="assets/plugins/select2/js/select2.min.js"></script>
 
-  <script src="assets/plugins/select2/js/select2.min.js"></script>
-
-  <script src="assets/js/script.js"></script>
+	<script src="assets/js/script.js"></script>
 </body>
 
 </html>
