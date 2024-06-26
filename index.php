751bfeeaad576aab5573072c9774a174067af783
Guheil <xgael.sanjuan@gmail.com>
2024-06-26 22:42:04 +0800

    Fixed Modals

--- index.php
+++ index.php
@@ -63,6 +63,25 @@ if ($result) {
 </head>
 
 <body>
+	<div class="modal fade" id="logoutModal" tabindex="-1" aria-labelledby="logoutModalLabel" aria-hidden="true">
+						<div class="modal-dialog">
+							<div class="modal-content">
+								<div class="modal-header">
+									<h5 class="modal-title" id="logoutModalLabel">Confirm Logout</h5>
+									<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+										<span aria-hidden="true">&times;</span>
+									</button>
+								</div>
+								<div class="modal-body">
+									Are you sure you want to logout?
+								</div>
+								<div class="modal-footer">
+									<button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+									<button type="button" class="btn btn-danger" id="confirmLogout">Logout</button>
+								</div>
+							</div>
+						</div>
+					</div>
 	<div class="main-wrapper">
 
 		<div class="header">
@@ -113,25 +132,7 @@ if ($result) {
 						<a class="dropdown-item" href="settings.html"><i data-feather="settings" class="mr-1"></i> Settings</a>
 						<a class="dropdown-item" href="#" id="logoutLink"><i data-feather="log-out" class="mr-1"></i> Logout</a>
 					</div>
-					<div class="modal fade" id="logoutModal" tabindex="-1" aria-labelledby="logoutModalLabel" aria-hidden="true">
-						<div class="modal-dialog">
-							<div class="modal-content">
-								<div class="modal-header">
-									<h5 class="modal-title" id="logoutModalLabel">Confirm Logout</h5>
-									<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-										<span aria-hidden="true">&times;</span>
-									</button>
-								</div>
-								<div class="modal-body">
-									Are you sure you want to logout?
-								</div>
-								<div class="modal-footer">
-									<button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
-									<button type="button" class="btn btn-danger" id="confirmLogout">Logout</button>
-								</div>
-							</div>
-						</div>
-					</div>
+					
 
 					<script>
 						document.getElementById('logoutLink').addEventListener('click', function(event) {
