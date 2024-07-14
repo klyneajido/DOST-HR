ddb2ca8602de9d81f05bd163a82a2f70a6caaaea
mairaRosales <rmairar518@gmail.com>
2024-07-15 00:15:58 +0800

    editable profile

--- profile.php
+++ profile.php
@@ -1,7 +1,7 @@
 <?php
 // Start session
 session_start();
-include_once 'PHP_Connections\db_connection.php';
+include_once 'PHP_Connections/db_connection.php';
 
 // Check if user is logged in
 if (!isset($_SESSION['username'])) {
@@ -10,9 +10,49 @@ if (!isset($_SESSION['username'])) {
     exit();
 }
 
-// Get user's name from session
 $user_name = isset($_SESSION['username']) ? $_SESSION['username'] : 'Guest';
 $profile_image_path = isset($_SESSION['profile_image']) ? $_SESSION['profile_image'] : 'assets/img/profiles/default-profile.png';
+
+// Get user's username from session
+$username = $_SESSION['username'];
+
+// Fetch admin details from the database
+$query = "SELECT name, username, email, profile_image FROM admins WHERE username = ?";
+$stmt = $mysqli->prepare($query);
+$stmt->bind_param('s', $username);
+$stmt->execute();
+$result = $stmt->get_result();
+
+if ($result->num_rows == 1) {
+    $admin = $result->fetch_assoc();
+} else {
+    echo "User not found.";
+    exit();
+}
+
+// If the form is submitted, update the profile details
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    // Handle profile image update if a new one is uploaded
+    if (!empty($_FILES['profile_image']['name'])) {
+        $profile_image = addslashes(file_get_contents($_FILES['profile_image']['tmp_name']));
+    } else {
+        $profile_image = $admin['profile_image'];
+    }
+
+    $update_query = "UPDATE admins SET name = ?, email = ?, profile_image = ? WHERE username = ?";
+    $update_stmt = $mysqli->prepare($update_query);
+    $update_stmt->bind_param('ssss', $name, $email, $profile_image, $username);
+    if ($update_stmt->execute()) {
+        $_SESSION['name'] = $name;
+        $_SESSION['email'] = $email;
+        $_SESSION['profile_image'] = $profile_image;
+        echo "<script>window.addEventListener('load', function() { $('#successModal').modal('show'); });</script>";
+    } else {
+        echo "Error updating profile.";
+    }
+}
 
 ?>
 <!DOCTYPE html>
@@ -21,75 +61,21 @@ $profile_image_path = isset($_SESSION['profile_image']) ? $_SESSION['profile_ima
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
     <title>HRMO Admin</title>
-
     <link rel="shortcut icon" href="assets/img/dost_logo.png">
-
     <link rel="stylesheet" href="assets/css/bootstrap.min.css">
-
     <link rel="stylesheet" href="assets/plugins/fontawesome/css/fontawesome.min.css">
     <link rel="stylesheet" href="assets/plugins/fontawesome/css/all.min.css">
     <link rel="stylesheet" href="assets/css/style.css">
-    <!-- [if lt IE 9]>
-			<script src="assets/js/html5shiv.min.js"></script>
-			<script src="assets/js/respond.min.js"></script>
-		<![endif] -->
 </head>
-
-<style>
-	#style-5::-webkit-scrollbar-track
-	{
-		-webkit-box-shadow: inset 0 0 6px rgba(0,0,0,0.3);
-		background-color: #F5F5F5;
-	}
-
-	#style-5::-webkit-scrollbar
-	{
-		width: 10px;
-		background-color: #F5F5F5;
-	}
-
-	#style-5::-webkit-scrollbar-thumb
-	{
-		background-color: #0ae;
-		
-		background-image: -webkit-gradient(linear, 0 0, 0 100%,
-											color-stop(.5, rgba(255, 255, 255, .2)),
-							color-stop(.5, transparent), to(transparent));
-	}
-</style>
-
 <body>
-    <div class="modal fade" id="logoutModal" tabindex="-1" aria-labelledby="logoutModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="logoutModalLabel">Confirm Logout</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    Are you sure you want to logout?
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
-                    <button type="button" class="btn btn-danger" id="confirmLogout">Logout</button>
-                </div>
-            </div>
-        </div>
-    </div>
     <div class="main-wrapper">
-
         <div class="header">
-
             <div class="header-left">
                 <div class="logo-wrapper">
                     <a href="index.php" class="logo">
                         <img src="assets/img/DOST.png" alt="Logo">
                     </a>
-
                 </div>
-
                 <a href="index.php" class="logo logo-small">
                     <img src="assets/img/dost_logo.png" alt="Logo" width="30" height="30">
                 </a>
@@ -101,85 +87,53 @@ $profile_image_path = isset($_SESSION['profile_image']) ? $_SESSION['profile_ima
                     </span>
                 </a>
             </div>
-
             <div class="top-nav-search" style="width:46.5%; margin-left:8%; min-width:20%;">
                 <form>
                     <input type="text" class="form-control" placeholder="">
                     <button class="btn" type="submit"><i class="fas fa-search"></i></button>
                 </form>
             </div>
-
-
             <a class="mobile_btn" id="mobile_btn">
                 <i class="fas fa-bars"></i>
             </a>
             <ul class="nav user-menu">
-
                 <li class="nav-item dropdown has-arrow main-drop">
                     <a href="#" class="dropdown-toggle nav-link" data-toggle="dropdown">
                         <span class="user-img">
                             <img src="<?php echo htmlspecialchars($profile_image_path); ?>" alt="Avatar" style="border-radius: 50%; width: 45px; height: 45px;">
                             <span class="status online"></span>
                         </span>
-                        <span><?php echo htmlspecialchars($user_name); ?></span>
+                        <span><?php echo htmlspecialchars($username); ?></span>
                     </a>
                     <div class="dropdown-menu">
-						<a class="dropdown-item" href="profile.html"><i data-feather="user" class="mr-1"></i> Profile</a>
-						<a class="dropdown-item" href="settings.html"><i data-feather="settings" class="mr-1"></i> Settings</a>
-						<a class="dropdown-item" href="#" id="logoutLink"><i data-feather="log-out" class="mr-1"></i> Logout</a>
-					</div>
-					
-
-					<script>
-						document.getElementById('logoutLink').addEventListener('click', function(event) {
-							event.preventDefault();
-							$('#logoutModal').modal('show');
-						});
-
-						document.getElementById('confirmLogout').addEventListener('click', function() {
-							window.location.href = 'PHP_Connections/logout.php';
-						});
-					</script>
-				</li>
+                        <a class="dropdown-item" href="profile.php"><i data-feather="user" class="mr-1"></i> Profile</a>
+                        <a class="dropdown-item" href="settings.php"><i data-feather="settings" class="mr-1"></i> Settings</a>
+                        <a class="dropdown-item" href="#" id="logoutLink"><i data-feather="log-out" class="mr-1"></i> Logout</a>
+                    </div>
+                    <script>
+                        document.getElementById('logoutLink').addEventListener('click', function(event) {
+                            event.preventDefault();
+                            $('#logoutModal').modal('show');
+                        });
+                        document.getElementById('confirmLogout').addEventListener('click', function() {
+                            window.location.href = 'PHP_Connections/logout.php';
+                        });
+                    </script>
                 </li>
-
             </ul>
             <div class="dropdown mobile-user-menu show">
                 <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" aria-expanded="false"><i class="fa fa-ellipsis-v"></i></a>
                 <div class="dropdown-menu dropdown-menu-right ">
                     <a class="dropdown-item" href="profile.php">My Profile</a>
-                    <a class="dropdown-item" href="settings.html">Settings</a>
+                    <a class="dropdown-item" href="settings.php">Settings</a>
                     <a class="dropdown-item" href="PHP_Connections/logout.php">Logout</a>
                 </div>
             </div>
-
         </div>
-
-
         <div class="sidebar" id="sidebar">
             <div class="sidebar-inner slimscroll">
                 <div class="sidebar-contents">
                     <div id="sidebar-menu" class="sidebar-menu">
-                        <div class="mobile-show">
-                            <div class="offcanvas-menu">
-                                <div class="user-info align-center bg-theme text-center">
-                                    <span class="lnr lnr-cross  text-white" id="mobile_btn_close">X</span>
-                                    <a href="javascript:void(0)" class="d-block menu-style text-white">
-                                        <div class="user-avatar d-inline-block mr-3">
-                                            <img src="<?php echo htmlspecialchars($profile_image_path); ?>" alt="user avatar" class="rounded-circle" width="50">
-                                        </div>
-                                    </a>
-                                </div>
-                            </div>
-                            <div class="sidebar-input">
-                                <div class="top-nav-search">
-                                    <form>
-                                        <input type="text" class="form-control" placeholder="Search here">
-                                        <button class="btn" type="submit"><i class="fas fa-search"></i></button>
-                                    </form>
-                                </div>
-                            </div>
-                        </div>
                         <ul>
                             <li>
                                 <a href="index.php"><img src="assets/img/home.svg" alt="sidebar_img">
@@ -201,17 +155,14 @@ $profile_image_path = isset($_SESSION['profile_image']) ? $_SESSION['profile_ima
                                 <a href="transparency.php"><img src="assets/img/employee.svg" alt="sidebar_img"><span>
                                         Transparency</span></a>
                             </li>
-
                             <li class="active">
                                 <a href="profile.php"><img src="assets/img/profile.svg" alt="sidebar_img">
                                     <span>Profile</span></a>
                             </li>
-                            
                         </ul>
                         <ul class="logout">
                             <li>
                                 <a href="#" id="sidebarLogoutLink"><img src="assets/img/logout.svg" alt="sidebar_img"><span>Log out</span></a>
-                                
                             </li>
                         </ul>
                     </div>
@@ -223,44 +174,138 @@ $profile_image_path = isset($_SESSION['profile_image']) ? $_SESSION['profile_ima
                 event.preventDefault();
                 $('#logoutModal').modal('show');
             });
-
             document.getElementById('confirmLogout').addEventListener('click', function() {
                 window.location.href = 'PHP_Connections/logout.php';
             });
         </script>
         <div class="page-wrapper">
             <div class="container-fluid">
-              <div class="breadcrumb-path mb-4 my-4" >
-                <ul class="breadcrumb">
-                  <li class="breadcrumb-item">
-                    <a href=""><img src="assets/img/dash.png" class="mr-2" alt="breadcrumb" />Profile</a>
-                  </li>
-                  <li class="breadcrumb-item active">Account</li>
-                </ul>
-                <div class="d-flex gap-3">
-                  
+                <div class="breadcrumb-path mb-4 my-4">
+                    <ul class="breadcrumb">
+                        <li class="breadcrumb-item">
+                            <a href="index.php"><img src="assets/img/dash.png" class="mr-2" alt="breadcrumb" />Profile</a>
+                        </li>
+                        <li class="breadcrumb-item active">Account</li>
+                    </ul>
                 </div>
-              </div>
+                <div class="card">
+                    <div class="card-body">
+                        <?php if (!empty($errors)) : ?>
+                            <div class="alert alert-danger">
+                                <ul>
+                                    <?php foreach ($errors as $error) : ?>
+                                        <li><?php echo htmlspecialchars($error); ?></li>
+                                    <?php endforeach; ?>
+                                </ul>
+                            </div>
+                        <?php endif; ?>
+                        <form method="POST" enctype="multipart/form-data" onsubmit="return confirmSubmission(event)">
+                            <div class="row">
+                                <!-- Profile Image and Upload Input on the left side -->
+                                <div class="col-md-4">
+                                    <div class="form-group text-center">
+                                        <label for="profile_image">Profile Image</label><br>
+                                        <?php if (!empty($admin['profile_image'])) : ?>
+                                            <img src="data:image/jpeg;base64,<?php echo base64_encode($admin['profile_image']); ?>" alt="Profile Image" style="max-width: 200px; max-height: 200px; display: block; margin: 0 auto;">
+                                        <?php else : ?>
+                                            <span>No image uploaded</span>
+                                        <?php endif; ?>
+                                        <br>
+                                        <input type="file" name="profile_image" id="profile_image" class="form-control-file mt-3" style="margin: 0 auto;">
+                                    </div>
+                                </div>
 
+                                <!-- Other form fields on the right side -->
+                                <div class="col-md-8">
+                                    <div class="form-group">
+                                        <label for="name">Name</label>
+                                        <input type="text" name="name" id="name" class="form-control" value="<?php echo htmlspecialchars($admin['name']); ?>" required>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="username">Username</label>
+                                        <input type="text" name="username" id="username" class="form-control" value="<?php echo htmlspecialchars($admin['username']); ?>" disabled>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="email">Email</label>
+                                        <input type="email" name="email" id="email" class="form-control" value="<?php echo htmlspecialchars($admin['email']); ?>" required>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="password">Password</label>
+                                        <input type="password" name="password" id="password" class="form-control" value="********" disabled>
+                                    </div>
+                                    <div class="col-md-12 offset-md-6">
+                                        <button type="submit" class="btn btn-primary py-3 w-25">Update Profile</button>
+                                        <a href="index.php" class="btn btn-secondary py-3 w-25">Cancel</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
             </div>
         </div>
-
     </div>
+
+    <!-- Confirmation Modal -->
+    <div class="modal fade" id="confirmationModal" tabindex="-1" role="dialog" aria-labelledby="confirmationModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="confirmationModalLabel">Confirm Update</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    Are you sure you want to update your profile?
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary py-2" data-dismiss="modal">Cancel</button>
+                    <button type="button" class="btn btn-primary w-25 py-2" id="confirmUpdate">Yes, Update</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Success Modal -->
+    <div class="modal fade" id="successModal" tabindex="-1" role="dialog" aria-labelledby="successModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="successModalLabel">Success</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    Your profile has been updated successfully.
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-primary" data-dismiss="modal">OK</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script>
+        function confirmSubmission(event) {
+            event.preventDefault();
+            $('#confirmationModal').modal('show');
+        }
+
+        document.getElementById('confirmUpdate').addEventListener('click', function() {
+            document.querySelector('form').submit();
+        });
+    </script>
+
     <script src="assets/js/date.js"></script>
     <script src="assets/js/jquery-3.6.0.min.js"></script>
-
     <script src="assets/js/popper.min.js"></script>
     <script src="assets/js/bootstrap.min.js"></script>
-
     <script src="assets/js/feather.min.js"></script>
-
     <script src="assets/plugins/slimscroll/jquery.slimscroll.min.js"></script>
-
     <script src="assets/plugins/apexchart/apexcharts.min.js"></script>
     <script src="assets/plugins/apexchart/chart-data.js"></script>
     <script src="assets/js/script.js"></script>
-    <!-- sdsadasdasd -->
-
 </body>
-
 </html>
