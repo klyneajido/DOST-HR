1746ce3d033d4a7b656faeb05bfb1dc6baf66cd6
Guheil <xgael.sanjuan@gmail.com>
2024-07-24 23:13:41 +0800

    try link

--- PHP_Connections/download_document.php
+++ PHP_Connections/download_document.php
@@ -6,7 +6,7 @@ include_once 'PHP_Connections/db_connection.php';
 // Check if user is logged in
 if (!isset($_SESSION['username'])) {
     // Redirect to login page if not logged in
-    header('Location: login.php');
+    header('Location: ../login.php');
     exit();
 }
 
